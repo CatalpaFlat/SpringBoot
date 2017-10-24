7120e5887714aa39f9ab2fabad152e1c4d7151c6
CatalpaFlat <1013427541@qq.com>
2017-10-24 14:02:47 +0800

    添加druid

---
+++ SpringBoot-Druid-Demo/src/main/java/com/chen/SpringbootDruidDemoApplication.java
@@ -0,0 +1,14 @@
+package com.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class SpringbootDruidDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDruidDemoApplication.class, args);
+	}
+}
---
+++ SpringBoot-Druid-Demo/src/main/java/com/chen/config/DruidStatFilter.java
@@ -0,0 +1,18 @@
+package com.chen.config;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * druid 过滤器
+ * Created by 陈梓平 on 2017/9/13.
+ */
+@WebFilter(filterName="druidWebStatFilter",urlPatterns="/*",
+        initParams={
+                @WebInitParam(name="exclusions",value="*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")// 忽略资源
+        }
+)
+public class DruidStatFilter extends WebStatFilter {
+}
