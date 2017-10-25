cce82afe601dd257057cde9731eb7a6648a00abd
CatalpaFlat <1013427541@qq.com>
2017-10-25 10:25:40 +0800

    添加websocket案例

---
+++ SpringBoot-Websocket-Demo/src/main/java/com/chen/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.chen.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * webSocket配置
+ * @author 陈梓平
+ * @date 2017/10/25.
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
